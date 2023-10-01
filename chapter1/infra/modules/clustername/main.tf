e493f8f8df73780774e8cc73c327c437f6740cca
salecharohit <i@rohitsalecha.com>
2023-10-01 15:12:19 +0530

    - reverting back for clustername

--- chapter1/infra/modules/clustername/main.tf
+++ chapter1/infra/modules/clustername/main.tf
@@ -1,8 +1,4 @@
-resource "random_id" "cluster_random_id" {
-  byte_length = 5
-}
-
-# Create a cluster-name with org region,environment and 5 byte random ID combination
+# Create a cluster-name with org region and environment combination
 locals {
-  cluster_name = "${var.org_name}-${random_id.cluster_random_id.hex}-${var.region}-${var.environment}"
+  cluster_name = "${var.org_name}-${var.region}-${var.environment}"
 }
