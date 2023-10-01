a8e67f4d03a11c1314f2954b583ecdf8eb0779d2
salecharohit <i@rohitsalecha.com>
2023-10-01 13:42:41 +0530

    - adding random id - updating DNS template record

--- chapter1/infra/modules/clustername/main.tf
+++ chapter1/infra/modules/clustername/main.tf
@@ -1,4 +1,8 @@
-# Create a cluster-name with org region and environment combination
+resource "random_id" "cluster_random_id" {
+  byte_length = 5
+}
+
+# Create a cluster-name with org region,environment and 5 byte random ID combination
 locals {
-  cluster_name = "${var.org_name}-${var.region}-${var.environment}"
+  cluster_name = "${var.org_name}-${random_id.cluster_random_id.hex}-${var.region}-${var.environment}"
 }
--- chapter1/infra/terraform.auto.tfvars
+++ chapter1/infra/terraform.auto.tfvars
@@ -34,4 +34,4 @@ db_maximum_storage      = 100
 
 eks_version    = "1.24"
 instance_types = ["t3.large"]
-domain         = "s4cp.rohitsalecha.com"
+domain         = "userX.s4cp.rohitsalecha.com"
